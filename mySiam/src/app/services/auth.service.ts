ca541483f0c2ed1a8e2b59e5ad6ad2048b872bfd
nemya10 <zeramdini-a@hotmail.fr>
2021-04-01 13:54:08 +0200

    cc

--- mySiam/src/app/services/auth.service.ts
+++ mySiam/src/app/services/auth.service.ts
@@ -20,9 +20,7 @@ export class AuthService {
   user: User;
   eventSourceR = [];
   eventSourceJ = [];
-  numberNonJeune: number = 0;
-  numberRecupere: number = 0;
-  numberNonJeuneNourrir: number = 0;
+
   useriud = ''
 
 
@@ -81,10 +79,8 @@ export class AuthService {
   getCurrentUser() {
     return this.afAuth.currentUser;
   }
-  editEvent(eventS) {
-    console.log('id edit ' + eventS.id);
-    console.log('title edit ' + eventS.title);
-    console.log('desc edit ' + eventS.desc);
+  
+  editEventMotif(eventS) {
 
     this.afDB.list('Events/' + this.useriud).update(eventS.id, {
       title: eventS.title,
@@ -92,6 +88,19 @@ export class AuthService {
       
 
     });
+  }
+  
+
+  editEventRecp(eventS) {
+    this.afDB.list('Events/' + this.useriud).update(eventS.id, {
+      title: 'Jour récupéré',
+      desc: '',
+    });
+  }
+  supprimerEvent(eventS) {
+
+
+    this.afDB.list('Events/' + this.useriud).remove(eventS.id);
   }
   async toast(message, status) {
     const toast = await this.toastr.create({
@@ -180,39 +189,17 @@ export class AuthService {
     this.afAuth.authState.subscribe(data => {
       this.useriud = data.uid;
       console.log('Events/' + this.useriud);
-      this.afDB.list('Events/' + data.uid).snapshotChanges(['child_added']).subscribe(actions => {
+        this.afDB.list('Events/' + data.uid).snapshotChanges(['child_added']).subscribe(actions => {
         this.eventSourceJ = [];
         this.eventSourceR = [];
+        
         actions.forEach(action => {
-          console.log('action:' + action.payload.exportVal().title);
-          console.log('boolean: ' + action.payload.exportVal().title == 'Jour récupéré');
-          console.log('direct: ' + action.payload.exportVal().startTime);
-          console.log('dat new: ' + new Date(action.payload.exportVal().startTime));
+          
           if (action.payload.exportVal().title == 'Jour non jeûné') {
-            if (action.payload.exportVal().desc == 'Maladie (courte durée)') {
-              this.numberNonJeuneNourrir++;
-            }
-            else if (action.payload.exportVal().desc == 'Maladie (longue durée donc irrattrapable)') {
-              this.numberNonJeuneNourrir++;
-            }
-            else if (action.payload.exportVal().desc == 'Grossesse') {
-              this.numberNonJeune++;
-            }
-            else if (action.payload.exportVal().desc == 'Allaitement') {
-              this.numberNonJeuneNourrir++;
-              this.numberNonJeune++;
-            }
-            else if (action.payload.exportVal().desc == 'Période de menstrue') {
-              this.numberNonJeune++;
-            }
-            else if (action.payload.exportVal().desc == 'Voyage') {
-              this.numberNonJeune++;
-            }
+           
             //  console.log('numberNonnnnnnn: '+  this.numberNonJeune);
             //  console.log('numberNonNourrrrrrrr: '+  this.numberNonJeuneNourrir);
-            console.log('iddddddddddddd: ' + action.key);
-
-            this.eventSourceJ.push({
+             this.eventSourceJ.push({
               id: action.key,
               title: action.payload.exportVal().title,
               startTime: action.payload.exportVal().startTime,
@@ -222,8 +209,6 @@ export class AuthService {
               eventColor: 'red'
             });
           } else {
-            console.log('idddddddddddddR: ' + action.key);
-            this.numberRecupere++;
             this.eventSourceR.push({
               id: action.key,
               title: action.payload.exportVal().title,
@@ -237,15 +222,10 @@ export class AuthService {
           }
 
         });
-        console.log('eventSourceR: ' + this.eventSourceR.keys);
-        console.log('eventSourceJ: ' + this.eventSourceJ.keys);
-
-        console.log('numberNonnnnnnn: ' + this.numberNonJeune);
-        console.log('numberNonNourrrrrrrr: ' + this.numberNonJeuneNourrir * 7);
-        console.log('numberRecup: ' + this.numberRecupere);
+        setTimeout((() => {console.log("fff")}),500);
+       
       });
     })
-    //this.numberNonJeuneNourrir = this.numberNonJeuneNourrir*7;
-    //console.log('numberNonNourrrrrrrrfiin: '+  this.numberNonJeuneNourrir);
+
  }
 }
